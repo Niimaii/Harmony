bac1f0813a4e4f5d0330eb79742c50f6b236cec2
Niimaii <nimaicgomes@gmail.com>
2024-07-26 04:06:16 -0300

    pushing so people can see the code that is error. Slash commands are not showing up in the discord sserver.

---
+++ src/deploy-commands.js
@@ -0,0 +1,56 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord.js';
+import { DISCORD_TOKEN, CLIENT_ID, GUILD_ID } from '../config.js';
+
+console.log('deploy-commands ran');
+function getFiles(dir) {
+  const files = fs.readdirSync(dir, {
+    withFileTypes: true,
+  });
+
+  let commandFiles = [];
+
+  //   Loop through all the files and folders in the `command` folder
+  for (const file of files) {
+    // If a folder is being looped over, run `getFiles()` recursively so that all the files in that folder is accounted for
+    if (file.isDirectory()) {
+      commandFiles = [...commandFiles, ...getFiles(`${dir}/${file.name}`)];
+    }
+    // If what is being looped over a file that ends with .js, then add the file path to `commandFiles`
+    else if (file.name.endsWith('.js')) {
+      commandFiles.push(`${dir}/${file.name}`);
+    }
+  }
+
+  return commandFiles;
+}
+
+async function loadCommands() {
+  let commands = [];
+  const commandFiles = getFiles('./src/commands');
+
+  for (const file of commandFiles) {
+    const command = await import(path.resolve(file));
+    //   Push all the commands within "data" in each file
+    commands.push(command.default.data.toJSON());
+  }
+
+  return commands;
+}
+
+export async function deployCommands() {
+  const commands = await loadCommands();
+  const rest = new REST({ version: '10' }).setToken(DISCORD_TOKEN);
+
+  // Run all the commands in our specific server
+  rest
+    .put(
+      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID, { body: commands })
+    )
+    .then(() =>
+      console.log('Successfully registered the application commands!')
+    )
+    .catch(console.error);
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import { Client, GatewayIntentBits, Routes } from 'discord.js';
+import {
+  Client,
+  Collection,
+  Events,
+  GatewayIntentBits,
+  Routes,
+} from 'discord.js';
 import { REST } from '@discordjs/rest';
 import { DISCORD_TOKEN } from '../config.js';
+import fs from 'node:fs';
+import path from 'node:path';
+import { deployCommands } from './deploy-commands.js';
 
 const client = new Client({
   intents: [
@@ -10,6 +19,61 @@ const client = new Client({
   ],
 });
 
-const rest = new REST({ version: '10' }).setToken(DISCORD_TOKEN);
+function getFiles(dir) {
+  const files = fs.readdirSync(dir, {
+    withFileTypes: true,
+  });
+
+  let commandFiles = [];
+
+  //   Loop through all the files and folders in the `command` folder
+  for (const file of files) {
+    // If a folder is being looped over, run `getFiles()` recursively so that all the files in that folder is accounted for
+    if (file.isDirectory()) {
+      commandFiles = [...commandFiles, ...getFiles(`${dir}/${file.name}`)];
+    }
+    // If what is being looped over a file that ends with .js, then add the file path to `commandFiles`
+    else if (file.name.endsWith('.js')) {
+      commandFiles.push(`${dir}/${file.name}`);
+    }
+  }
+
+  return commandFiles;
+}
+
+async function getCommands(dir) {
+  let commands = new Collection();
+  const commandFiles = getFiles(dir);
+
+  for (const commandFile of commandFiles) {
+    const command = await import(path.resolve(commandFile));
+    commands.set(command.default.data.toJSON().name, command.default);
+  }
+  return commands;
+}
+
+deployCommands();
+
+client.commands = getCommands('./src/commands');
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`${c.user.tag} logged in`);
+});
+client.on(Events.InteractionCreate, (interaction) => {
+  console.log('Client.on ran');
+
+  if (!interaction.isChatInputCommand()) return;
+
+  console.log(`Command received: ${interaction.commandName}`);
+
+  let command = client.commands.get(interaction.commandName);
+
+  try {
+    if (interaction.replied) return;
+    command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 client.login(DISCORD_TOKEN);
