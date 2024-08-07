5095126c632b83ace2520d7738887b7b6a6a1a34
Niimaii <nimaicgomes@gmail.com>
2024-08-06 23:55:47 -0300

    Fixed bug caused by folder restructure

--- src/discordjs/deploy-commands.js
+++ src/discordjs/deploy-commands.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 
 const commands = [];
 // Grab all the command folders from the commands directory you created earlier
-const foldersPath = path.join(__dirname, 'commands');
+const foldersPath = path.join(__dirname, 'discordjs/commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
